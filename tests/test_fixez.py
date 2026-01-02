33b23b5d2287c643e29b4e4d7a847fa65c97779c
Brytonb16 <brookebryton@gmail.com>
2026-01-02 13:55:43 -0800

    Tighten Fixez query relevance filter

--- tests/test_fixez.py
+++ tests/test_fixez.py
@@ -23,3 +23,49 @@ def test_scrape_fixez_filters_irrelevant_results(monkeypatch):
 
     assert len(results) == 1
     assert results[0]["title"] == "iPhone 13 Screen"
+
+
+def test_matches_query_prefers_high_overlap(monkeypatch):
+    html = """
+    <ul>
+      <li class="product-item">
+        <a class="product-item-link" href="/switch-case">Nintendo Switch Carry Case</a>
+        <span class="price">$19.99</span>
+        <img src="//images/case.jpg" />
+      </li>
+      <li class="product-item">
+        <a class="product-item-link" href="/joycons">Nintendo Switch Joy-Con (L/R)</a>
+        <span class="price">$79.99</span>
+        <img src="//images/joycon.jpg" />
+      </li>
+    </ul>
+    """
+
+    monkeypatch.setattr(fixez, "render_page", lambda *_args, **_kwargs: html)
+
+    results = fixez.scrape_fixez("Nintendo Switch Joy Cons")
+
+    assert len(results) == 1
+    assert "Joy-Con" in results[0]["title"]
+
+
+def test_scrape_fixez_drops_accessory_kits(monkeypatch):
+    html = """
+    <ul>
+      <li class="product-item">
+        <a class="product-item-link" href="/switch-screws">Nintendo Switch Joy-Con Screw Set</a>
+        <span class="price">$4.99</span>
+      </li>
+      <li class="product-item">
+        <a class="product-item-link" href="/switch-joycon">Nintendo Switch Joy-Con (L/R)</a>
+        <span class="price">$79.99</span>
+      </li>
+    </ul>
+    """
+
+    monkeypatch.setattr(fixez, "render_page", lambda *_args, **_kwargs: html)
+
+    results = fixez.scrape_fixez("Nintendo Switch Joy Cons")
+
+    assert len(results) == 1
+    assert results[0]["title"] == "Nintendo Switch Joy-Con (L/R)"
